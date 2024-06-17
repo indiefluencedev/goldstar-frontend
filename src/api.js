9b2cbcd7fee0ade90a0ac107eafb5f0a8dd295bc
anurag262000 <anuragmishra262000@gmail.com>
2024-06-17 16:37:52 +0530

    commit

--- src/api.js
+++ src/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const axiosInstance = axios.create({
-  baseURL: 'https://testing-backend-s0dg.onrender.com/api',
+  baseURL: 'https://testing-backend-s0dg.onrender.com',
   withCredentials: true // Include credentials with requests
 });
 
