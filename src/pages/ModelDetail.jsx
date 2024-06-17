2980ee7832868c5eb95b4131bd720468d60078ef
anurag262000 <anuragmishra262000@gmail.com>
2024-06-17 15:55:24 +0530

    ui update

--- src/pages/ModelDetail.jsx
+++ src/pages/ModelDetail.jsx
@@ -53,7 +53,7 @@ const ModelDetails = ({ addToCompare, compareList }) => {
             setError(null);
 
             try {
-                const response = await axios.get(`http://localhost:8001/api/models/${modelType}/${modelId}`);
+                const response = await axios.get(`https://testing-backend-s0dg.onrender.com/models/${modelType}/${modelId}`);
                 console.log('Response data:', response.data);
                 setModelDetails(response.data);
                 if (response.data.series && response.data.series.name) {
