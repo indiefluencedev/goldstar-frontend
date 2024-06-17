a81bb34ea4a033dfc0f5420d8815f2a619888a8d
anurag262000 <anuragmishra262000@gmail.com>
2024-06-17 17:33:44 +0530

    take5

--- src/pages/Categories.jsx
+++ src/pages/Categories.jsx
@@ -1,3 +1,4 @@
+// src/pages/Categories.js
 import React, { useEffect, useState, useContext } from 'react';
 import { useParams, useNavigate } from 'react-router-dom';
 import { getSeriesById } from '../services/api';
