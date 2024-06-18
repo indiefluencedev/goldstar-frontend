c222c41e451f6684c6b6757023c7002c20b5511e
anurag262000 <anuragmishra262000@gmail.com>
2024-06-18 15:22:59 +0530

    header

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -74,7 +74,7 @@ const NavBar = () => {
                     <span className="sr-only">Open main menu</span>
                     {!nav ? <Bars3Icon className="w-6" /> : <XMarkIcon className="w-6" />}
                 </button>
-                <div id="mega-menu-full" className={`items-center md:ml-[200px] xl:ml-[400px] justify-between font-medium ${nav ? 'block' : 'hidden'} w-full md:flex md:w-auto md:order-1`}>
+                <div id="mega-menu-full" className={`items-center md:ml-[200px] xl:ml-[600px] justify-between font-medium ${nav ? 'block' : 'hidden'} w-full md:flex md:w-auto md:order-1`}>
                     <ul className="flex flex-col p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-white md:space-x-8 rtl:space-x-reverse md:flex-row md:mt-0 md:border-0 font-assistant">
                         <li>
                             <Link to="/" className="block py-2 px-3 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-prime md:p-0 text-black font-assistant" aria-current="page">
