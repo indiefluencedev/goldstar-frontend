b00d1c63f0b2d2f73a3bb078f0fef3b13f7557d2
anurag262000 <anuragmishra262000@gmail.com>
2024-08-06 14:53:35 +0530

    link update

--- src/api.js
+++ src/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const instance = axios.create({
-  baseURL: 'http://localhost:8001', // Replace with your backend URL
+  baseURL: 'https://goldstar-backend.onrender.com', // Replace with your backend URL
   withCredentials: true, // Ensure credentials are included
 });
 
