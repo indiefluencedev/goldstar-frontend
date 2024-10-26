0c03cb1376084832e427c5e5d449b4d35e7fe128
indiefluence <indiefluence.in.media@gmail.com>
2024-10-26 16:30:05 +0530

    google analytics tracking added in contactus.jsx

--- src/pages/Contactus.jsx
+++ src/pages/Contactus.jsx
@@ -1,10 +1,16 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 
 import Contacts from '../components/contactus/Contacts';
 import MetaTag from '../utils/meta';
 import { metaData } from '../utils/metaData';
+import trackPageView from '../utils/tracking';
 
 const ContactUs = () => {
+
+    useEffect(() => {
+        trackPageView("/contact", "Contact Page");
+    }, []);
+
     return (
         <>
         <MetaTag title={metaData.contact.title} />
