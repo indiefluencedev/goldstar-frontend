db6d432a8c656feca8f816ea1a21976340df8d29
indiefluence <indiefluence.in.media@gmail.com>
2024-10-26 16:45:32 +0530

    google analytics tracking added in NotFound.jsx

--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -1,7 +1,16 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
+import React, { useEffect } from 'react'
+import { Link, useLocation } from 'react-router-dom'
+import trackPageView from '../utils/tracking';
 
 export default function Notfound() {
+  
+  const location = useLocation();
+
+  useEffect(() => {
+    const pagePath = location.pathname + location.search;
+    trackPageView( pagePath, "404 - Page Not Found");
+}, []);
+
   return (
     <div className="flex flex-col h-screen justify-center items-center bg-gray-100">
     <div className="flex flex-col items-center">
